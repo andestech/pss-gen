b1b0df018aa8ed894657e9207c7b4f64a96d26ed
kaizsv <kai.zsv@gmail.com>
2023-04-28 22:26:22 +0800

    Improve List data type
    and test list in samples/test_collections

--- src/PSSListInst.java
+++ src/PSSListInst.java
@@ -1,26 +1,33 @@
 import java.util.*;
 
 public class PSSListInst extends PSSInst {
+    PSSModel m_type_model;
 	ArrayList<PSSInst> m_list;
 
 	PSSListInst(String id, PSSModel type_model, boolean rand) {
 		super(id, "list<"+type_model.m_id+">", rand);
+        super.m_rand = false;
+        m_type_model = type_model;
 		m_list = new ArrayList<PSSInst>();
 	}
 
 	public void assign(PSSVal val) {
-		for (int i=0; i<m_list.size(); i++) {
-			PSSInst item = m_list.get(i);
-			PSSVal item_val = val.indexOf(i);
-			item.assign(item_val);
-		}
+        if (!PSSSetVal.class.isInstance(val))
+            PSSMessage.Fatal("The list type should be assigned as a list_literal.");
+
+        PSSSetVal valset = (PSSSetVal) val;
+        m_list.clear();
+        for (int i = 0; i < valset.getValList().size(); i++) {
+            PSSInst item_inst = m_type_model.declInst("", m_rand);
+            PSSVal item_val = val.indexOf(i);
+            item_inst.assign(item_val);
+            m_list.add(item_inst);
+        }
 	}
 
 	public void randomize() {
-		for (int i=0; i<m_list.size(); i++) {
-			PSSInst item = m_list.get(i);
-			item.randomize();
-		}
+        // PSS Standard V2.0 - 8.1 Data types General
+        // The list collection type is not randomizable.
 	}
 
 	public PSSInst indexOf(int index) {
--- src/PSSListModel.java
+++ src/PSSListModel.java
@@ -5,7 +5,7 @@ public class PSSListModel extends PSSModel {
 	PSSModel m_type_model;
 
 	public PSSListModel (PSSModel type_model) {
-		super("array<"+type_model.getText()+">");
+		super("list<"+type_model.getText()+">");
 		m_type_model = type_model;
 	}
 	public PSSListInst declInst(String id, boolean rand) {
