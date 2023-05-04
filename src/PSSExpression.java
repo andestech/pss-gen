f4cf72e3c664046fea86b6895651ad8d303993bf
Ming-Hsien Tsai <mhtsai208@gmail.com>
2023-05-04 16:15:27 +0800

    Add some comments.

--- src/PSSExpression.java
+++ src/PSSExpression.java
@@ -1,24 +1,47 @@
-import java.util.*;
 
 public class PSSExpression {
 
 	public PSSExpression() {
-		;
 	}
 
+	/**
+	 * Infers the domains of variables that satisfy this expression.
+	 *
+	 * @param var
+	 * @return domains successfully inferred
+	 */
 	public PSSDomainMap deduceDomain(PSSInst var) {
 		PSSMessage.Fatal(getClass().getSimpleName() + ".deduceDomain() is not implemented");
 		return null;
 	}
 
+	/**
+	 * Evaluates this expression.
+	 *
+	 * @param var
+	 * @return the evaluation result
+	 */
 	public PSSVal eval(PSSInst var) {
 		PSSMessage.Fatal(getClass().getSimpleName() + ".eval() is not implemented");
 		return null;
 	}
+
+	/**
+	 * Returns the instance of this expression.
+	 *
+	 * @param var
+	 * @return the instance of this expression, or {@code null} if this expression
+	 *         is not a variable
+	 */
 	public PSSInst getInst(PSSInst var) {
 		return null;
 	}
 
+	/**
+	 * Returns the text representation of this expression.
+	 *
+	 * @return the text representation of this expression
+	 */
 	public String getText() {
 		return "";
 	}
