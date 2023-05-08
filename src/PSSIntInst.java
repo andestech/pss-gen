13df5cd33f451dead796b2d9897664cdabf0f4d1
Ming-Hsien Tsai <mhtsai208@gmail.com>
2023-05-08 09:24:31 +0800

    Make the default value of the bit and int types 0.

--- src/PSSIntInst.java
+++ src/PSSIntInst.java
@@ -1,34 +1,41 @@
-import java.util.*;
 import java.math.*;
 
 public class PSSIntInst extends PSSInst {
+
+	/** the default value of bit and int types */
+	public static final PSSIntVal DEFAULT_VALUE = new PSSIntVal(0);
+
 	PSSIntVal m_val;
+
 	int m_width;
+
 	boolean m_sign;
 
-	PSSIntDomain m_domain;	// Solution domain
+	PSSIntDomain m_domain; // Solution domain
 
-	public PSSIntInst (String id, boolean rand, int width, boolean sign) {
+	public PSSIntInst(String id, boolean rand, int width, boolean sign) {
 		super(id, "int", rand);
+		m_val = DEFAULT_VALUE; // PSS v2.0 8.2.1: the default value of the bit and int types is 0
 		m_width = width;
 		m_sign = sign;
 		m_domain = new PSSIntDomain(width, sign);
 	}
+
 	public PSSIntDomain getInitDomain() {
 		return new PSSIntDomain(m_width, m_sign);
 	}
+
 	public PSSVal toVal() {
-		if (!m_initialized) {
-			PSSMessage.Error("IntInst", "'" + getHierarchyId() + "' is not initialized");
-		}
 		return m_val;
 	}
+
 	public boolean validate() {
 		if (!m_domain.InRange(m_val).toBool()) {
 			return false;
 		}
 		return true;
 	}
+
 	public void randomize() {
 		if (m_rand) {
 			boolean validated = false;
@@ -38,22 +45,25 @@ public class PSSIntInst extends PSSInst {
 				PSSRandom.limitRetry(retry);
 				randomize_one();
 				validated = validate();
-				retry ++;
+				retry++;
 			}
 			m_initialized = true;
 		}
 	}
+
 	public void randomize_one() {
 		BigInteger rand_val = m_domain.random().toBigInteger();
 		m_val = new PSSIntVal(rand_val);
-		//PSSMessage.Debug("randomize_one: val = " + m_val.toInt());
+		// PSSMessage.Debug("randomize_one: val = " + m_val.toInt());
 	}
+
 	public void assignDomain(PSSDomain domain) {
 		m_domain = (PSSIntDomain) domain;
 		if (m_domain.isEmpty()) {
 			PSSMessage.Error("OVERCONSTRAINT", "'" + getHierarchyId() + "' is over-constrained");
 		}
 	}
+
 	public void assign(PSSVal val) {
 		m_initialized = true;
 		BigInteger int_val = val.toBigInteger();
