d654cbb584e4427607f01508701df16dca4bbebc
Ming-Hsien Tsai <mhtsai208@gmail.com>
2023-05-16 12:02:04 +0800

    Add a new namespace in PSSForeachProcStmt.

--- src/PSSForeachProcStmt.java
+++ src/PSSForeachProcStmt.java
@@ -34,31 +34,35 @@ public class PSSForeachProcStmt extends PSSProcStmt {
         PSSIIterator it = c.iterator();
         PSSInst key_inst = null;
         PSSInst val_inst = null;
+        /* Create a new namespace to shadow the key and value variables */
+        PSSNamespaceInst foreach_inst = new PSSNamespaceInst("");
+        inst.addInst(foreach_inst);
         while (it.hasNext()) {
             Map.Entry<PSSVal, PSSVal> entry = it.next();
             PSSVal key = entry.getKey();
             PSSVal val = entry.getValue();
+
             if (m_index_identifier != null) {
                 if (!c.isIndexable())
                     PSSMessage.Error("", e.getText() + " is not indexable.");
                 if (key == null)
                     PSSMessage.Error("", "Expression " + m_expression.getText() + " does not have index variables");
                 if (key_inst == null) {
-                    key.getTypeModel().declInst(inst, m_index_identifier, false);
-                    key_inst = inst.findInstance(m_index_identifier);
+                    key.getTypeModel().declInst(foreach_inst, m_index_identifier, false);
+                    key_inst = foreach_inst.findInstance(m_index_identifier);
                     key_inst.setReadOnly(true);
                 }
                 key_inst.assign(key);
             }
             if (m_iterator_identifier != null) {
                 if (val_inst == null) {
-                    val.getTypeModel().declInst(inst, m_iterator_identifier, false);
-                    val_inst = inst.findInstance(m_iterator_identifier);
+                    val.getTypeModel().declInst(foreach_inst, m_iterator_identifier, false);
+                    val_inst = foreach_inst.findInstance(m_iterator_identifier);
                     val_inst.setReadOnly(true);
                 }
                 val_inst.assign(val);
             }
-            m_proc_stmt.eval(inst);
+            m_proc_stmt.eval(foreach_inst);
         }
     }
 
