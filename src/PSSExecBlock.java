a0445ccb12380099e7948da51b3bc5a87575865e
Ming-Hsien Tsai <mhtsai208@gmail.com>
2023-05-26 11:52:16 +0800

    Catch early return in exec blocks.

--- src/PSSExecBlock.java
+++ src/PSSExecBlock.java
@@ -4,8 +4,8 @@ public class PSSExecBlock extends PSSModel {
 
 	ArrayList<PSSProcStmt> m_list;
 	String m_kind;
-	
-	public PSSExecBlock (String kind) {
+
+	public PSSExecBlock(String kind) {
 		super("");
 		m_kind = kind;
 		m_list = new ArrayList<PSSProcStmt>();
@@ -24,18 +24,21 @@ public class PSSExecBlock extends PSSModel {
 		// Add an anonymous instance for exec local data
 		PSSInst block_inst = new PSSInst("", "", false);
 		inst.addInst(block_inst);
-		for (int i=0; i<m_list.size(); i++) {
-			PSSProcStmt stmt = m_list.get(i);
-			stmt.eval(block_inst);
+		try {
+			for (int i = 0; i < m_list.size(); i++) {
+				PSSProcStmt stmt = m_list.get(i);
+				stmt.eval(block_inst);
+			}
+		} catch (PSSReturnException e) {
 		}
 		inst.removeInst(block_inst);
 	}
 
-	public void dump (String indent) {
+	public void dump(String indent) {
 		System.out.println(indent + "exec " + m_kind + " {");
-		for (int i=0; i<m_list.size(); i++) {
+		for (int i = 0; i < m_list.size(); i++) {
 			PSSProcStmt stmt = m_list.get(i);
-			stmt.dump(indent+"\t");
+			stmt.dump(indent + "\t");
 		}
 		System.out.println(indent + "}");
 	}
