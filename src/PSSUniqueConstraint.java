0a28dc7274cc655e2fd764fe15ffc37434b6afd5
Luther Kai-Xuan Lee <luthe590@andestech.com>
2025-12-22 19:22:00 +0800

    [PSSMessage] Add getText() method

--- src/PSSUniqueConstraint.java
+++ src/PSSUniqueConstraint.java
@@ -31,6 +31,18 @@ public class PSSUniqueConstraint extends PSSConstraint {
 		return true;
 	}
 
+	public String getText() {
+		StringBuilder sb = new StringBuilder("unique { ");
+		for (int i = 0; i < m_id_list.size(); i++) {
+			if (i > 0) {
+				sb.append(", ");
+			}
+			sb.append(m_id_list.get(i).toString());
+		}
+		sb.append(" }");
+		return sb.toString();
+	}
+
 	public void dump(String indent) {
 		System.out.print(indent + "unique {");
 		for (PSSMemberPathElemExpression item : m_id_list) {
